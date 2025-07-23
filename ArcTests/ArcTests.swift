59c887e9831a4226249cf06b825eb5efe38c58b8
Khi Kidman <khi.kidman@gmail.com>
2025-07-23 15:09:48 -0500

    Initial Commit

---
+++ ArcTests/ArcTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArcTests.swift
+//  ArcTests
+//
+//  Created by Khi Kidman on 7/23/25.
+//
+
+import Testing
+@testable import Arc
+
+struct ArcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
